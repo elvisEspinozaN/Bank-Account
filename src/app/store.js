daad21ca0c1959f0c5d6c6f3f8114215188d9059
elvisespinozaN <elvis.espinoza.navarrete@outlook.com>
2025-03-05 21:53:52 -0500

    implement sessionStorage

--- src/app/store.js
+++ src/app/store.js
@@ -1,11 +1,41 @@
 import { configureStore } from "@reduxjs/toolkit";
 import transactionsReducer from "../features/transactions/transactionsSlice";
 
-// TODO: Configure the store to use the reducer from the transactions slice.
+// load state from session storage
+const loadState = () => {
+  try {
+    const serializedState = sessionStorage.getItem("bankState");
+    return serializedState ? JSON.parse(serializedState) : undefined;
+  } catch (e) {
+    console.error("Error loading state from sessionStorage", e);
+    return undefined;
+  }
+};
+
+// save state to session storage
+const saveState = (state) => {
+  try {
+    const serializedState = JSON.stringify(state);
+    sessionStorage.setItem("bankState", serializedState);
+  } catch (e) {
+    console.error("Error saving state to sessionStorage", e);
+  }
+};
+
+// load persisted state
+const persistedState = loadState();
+
 const store = configureStore({
   reducer: {
     transactions: transactionsReducer,
   },
+
+  preloadedState: persistedState,
+});
+
+// subscribe store changes and save state to sessionStorage
+store.subscribe(() => {
+  saveState(store.getState());
 });
 
 export default store;
